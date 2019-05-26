8f7a599022307f66d49c98cb7403a0eb02990171
Josip65 <jerkovic.josip5@gmail.com>
2019-05-26 18:20:31 +0200

    Here we go!

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">Početak muahahaha</div>
+
+                    <div class="card-body">
+                        Ovo je nekakav primjer teksta,, trebao bih malo proći vue js i laravel
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
